e538952b2c247d3f83a45c239509290ddc2c1783
Sion Cerezo <syon.dev@outlook.com>
2021-03-09 20:47:18 -0600

    add css y js pagina master

---
+++ resources/js/master.js
@@ -0,0 +1,10 @@
+window.$ = window.jQuery = require('../adminmart/assets/libs/jquery/dist/jquery.min.js')
+require('../adminmart/assets/libs/popper.js/dist/umd/popper.min.js')
+require('../adminmart/assets/libs/bootstrap/dist/js/bootstrap.min.js')
+// apps
+require('../adminmart/dist/js/app-style-switcher.js')
+window.feather = require('../adminmart/dist/js/feather.min.js')
+require('../adminmart/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js')
+require('../adminmart/dist/js/sidebarmenu.js')
+// Custom JavaScript
+require('../adminmart/dist/js/custom.js')
