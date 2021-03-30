e47bc6067985f03617ff095ccb6b7b8d194ef04b
Sion Cerezo <syon.dev@outlook.com>
2021-03-30 02:15:20 -0600

    add helper property

---
+++ config/properties/rules.php
@@ -0,0 +1,34 @@
+<?php
+return [
+    'alumno' => [
+        'nombre' => [
+            'size' => 100,
+            'type' => 'string',
+        ],
+        'ap_paterno' => [
+            'size' => 100,
+            'type' => 'string',
+        ],
+        'ap_materno' => [
+            'size' => 100,
+            'type' => 'string',
+        ],
+        'matricula' => [
+            'size' => 15,
+            'type' => 'string',
+        ],
+        'email' => [
+            'size' => 255,
+            'type' => 'email',
+        ],
+        'telefono' => [
+            'size' => 15,
+            'type' => 'string',
+        ],
+        'password' => [
+            'size' => 255,
+            'type' => 'string',
+        ],
+    ]
+]
+?>
